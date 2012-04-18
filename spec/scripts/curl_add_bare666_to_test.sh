feb6d29943b37b7bc6fcfd854723820f92ce1511
Naomi Dushay <ndushay@stanford.edu>
2012-04-18 10:41:46 -0700

    use port 8984 for testing to avoid clash with blacklight demo

--- spec/scripts/curl_add_bare666_to_test.sh
+++ spec/scripts/curl_add_bare666_to_test.sh
@@ -1,1 +1,1 @@
-curl "http://localhost:8983/solr/test/update?commit=true" -H "Content-Type: text/xml" --data-binary @spec/fixtures/add_solrdoc_bare666.xml
+curl "http://localhost:8984/solr/test/update?commit=true" -H "Content-Type: text/xml" --data-binary @spec/fixtures/add_solrdoc_bare666.xml
