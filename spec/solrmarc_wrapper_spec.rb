194fe15e09c104b0c592f17b8009d243423b5dcb
Naomi Dushay <ndushay@stanford.edu>
2014-06-06 13:27:31 -0700

    avoid rspec 3 deprecation warnings

--- spec/solrmarc_wrapper_spec.rb
+++ spec/solrmarc_wrapper_spec.rb
@@ -8,22 +8,22 @@ describe SolrmarcWrapper do
     @solrmarc_wrapper = SolrmarcWrapper.new(@@settings.solrmarc_dist_dir, @@settings.solrmarc_conf_props_file, @@settings.solr_url)
   end
   
-  it "should retrieve the SolrInputDoc generated from the marc record" do
+  it "retrieves the SolrInputDoc generated from the marc record" do
     sid = @solrmarc_wrapper.get_solr_input_doc_from_marcxml("666")
-    sid.should be_an_instance_of(Java::OrgApacheSolrCommon::SolrInputDocument)
+    expect(sid).to be_an_instance_of(Java::OrgApacheSolrCommon::SolrInputDocument)
     sid["id"].getValue.should == "666"
     sid["title_full_display"].getValue.should_not be_nil
   end
   
-  it "should have a SolrInputDoc with the non-stored fields present" do
+  it "has a SolrInputDoc with the non-stored fields present" do
     sid = @solrmarc_wrapper.get_solr_input_doc_from_marcxml("666")
     sid["title_245a_search"].getValue.should_not be_nil
   end
   
-  it "should log an error message when there is no document in the Solr index for the ckey" do
+  it "logs an error message when there is no document in the Solr index for the ckey" do
     lager = double("logger")
     @solrmarc_wrapper.logger = lager
-    lager.should_receive(:error).with("Can't find single Solr document with id aaa")
+    expect(lager).to receive(:error).with("Can't find single Solr document with id aaa")
     @solrmarc_wrapper.get_solr_input_doc_from_marcxml("aaa")
   end
   
