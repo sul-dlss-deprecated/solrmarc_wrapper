814a17f363e1c1750b0d79cd8ff59eb65ef7326a
Naomi Dushay <ndushay@stanford.edu>
2014-06-06 15:22:39 -0700

    ensure we're not using streaming solr server

--- lib/solrmarc_wrapper.rb
+++ lib/solrmarc_wrapper.rb
@@ -53,7 +53,7 @@ protected
   # @param solr_url the url of the Solr server
   # @param config_props_fname  the name of the xx_config.properties file relative to the solr_marc_dir used in initialize method
   def setup_solr_reindexer(solr_url, config_props_fname)
-    solr_core_loader = org.solrmarc.solr.SolrCoreLoader.loadRemoteSolrServer(solr_url, false, true)
+    solr_core_loader = org.solrmarc.solr.SolrCoreLoader.loadRemoteSolrServer(solr_url, false, false)
     @solrmarc_reindexer = org.solrmarc.marc.SolrReIndexer.new(solr_core_loader)
     @solrmarc_reindexer.init([config_props_fname])
   end
