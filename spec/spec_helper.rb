e618e8a029c3d86e2b8d13b01764727eb42241ee
Naomi Dushay <ndushay@stanford.edu>
2014-06-06 15:35:52 -0700

    add coveralls for travis build

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,12 @@
+require 'coveralls'
+Coveralls.wear!
+
 # for test coverage
 require 'simplecov'
-require 'simplecov-rcov'
-class SimpleCov::Formatter::MergedFormatter
-  def format(result)
-     SimpleCov::Formatter::HTMLFormatter.new.format(result)
-     SimpleCov::Formatter::RcovFormatter.new.format(result)
-  end
-end
-SimpleCov.formatter = SimpleCov::Formatter::MergedFormatter
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
 SimpleCov.start do
-  add_filter "/spec/"
-  add_filter "/config/"
+  add_filter "spec"
+  add_filter "config"
 end
 
 require 'solrmarc_wrapper/settings'
